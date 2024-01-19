9efbf9e93883b8692eb95ef0fa72c706df6c227a
Ab Reitsma <ab.reitsma@politie.nl>
2024-01-19 15:04:18 +0100

    initial version

---
+++ projects/ngx-simple-serializer/src/lib/serialize.spec.ts
@@ -0,0 +1,151 @@
+// import { LocalPersist } from './persist';
+import { serialize, deserialize, Serializable, CustomSerializer, SerializeSymbol, DeserializeSymbol, isSerializable } from './serialize';
+
+// *************************************************************************************
+// ** Testing if the serialization works
+// *************************************************************************************
+
+@Serializable('TestClass1')
+class TestSerializeNamedClass1 {
+  property1 = '123';
+}
+
+@Serializable()
+class TestSerializeClass1 {
+  property0 = null;
+  property1 = true;
+  property2 = 12345;
+  property3 = '123';
+  property4 = { a: 0 };
+  property5 = [0, '1'];
+  property6 = new Map([['key1', 'val1'], ['key2', 'val2']]);
+  property7 = new Set(['val1', 'val2', 'val3']);
+  property8 = new Date();
+  property9 = /lorem-ipsum/gi;
+
+  getProperty1() {
+    return this.property1;
+  }
+}
+
+@Serializable()
+class TestSerializeClass1b {
+  property0 = undefined; // special test case
+}
+
+@Serializable()
+class TestSerializeClass2 {
+  property1 = '123';
+  property2 = new TestSerializeClass1();
+  property3 = 12345;
+
+  getProperty2() {
+    return this.property2;
+  }
+}
+
+@Serializable()
+class TestSerializeClass3 implements CustomSerializer {
+  property1 = 'justatest';
+  property2 = 54321;
+
+  [SerializeSymbol](): any {
+    return {
+      p1: this.property1,
+      p2: this.property2
+    };
+  }
+
+  [DeserializeSymbol](value: any): TestSerializeClass3 {
+    const result = new TestSerializeClass3();
+    result.property1 = value.p1;
+    result.property2 = value.p2;
+    return result;
+  }
+}
+
+class TestUnserializableClass4 {
+  property1 = '123';
+  property2 = true;
+  property3 = 12345;
+  property4 = new Date();
+  property5 = /lorem-ipsum/gi;
+
+  getProperty1() {
+    return this.property1;
+  }
+}
+
+describe('serialize and deserialize', () => {
+  it('should serialize and deserialize @Serializable() class TestSerializeClass1', () => {
+    const testValue = new TestSerializeClass1();
+    const serializedValue = serialize(testValue);
+
+    expect(deserialize(serializedValue)).toEqual(testValue);
+  });
+
+  it('should serialize and deserialize @Serializable() class TestSerializeClass0 (special case for undefined)', () => {
+    const testValue = new TestSerializeClass1b();
+    const serializedValue = serialize(testValue);
+
+    expect(deserialize(serializedValue).property0).toEqual(testValue.property0);
+  });
+
+  it('should serialize a deserialize @Serializable(`TestClass1`) class TestSerializeNamedClass1', () => {
+    const testValue = new TestSerializeNamedClass1();
+    const serializedValue = serialize(testValue);
+
+    const content = JSON.parse(serializedValue);
+    expect(content._class).toEqual('TestClass1');
+
+    expect(deserialize(serializedValue)).toEqual(testValue);
+  });
+
+  it('should serialize and deserialize embedded @Serializable() classes', () => {
+    const testValue = new TestSerializeClass2();
+    const serializedValue = serialize(testValue);
+
+    expect(deserialize(serializedValue)).toEqual(testValue);
+  });
+
+  it('should serialize and deserialize classes with @Serializable decorator that implement CustomSerializer', () => {
+    const testValue = new TestSerializeClass3();
+    const serializedValue = serialize(testValue);
+
+    expect(deserialize(serializedValue)).toEqual(testValue);
+  });
+
+  it('should return an error calling deserialize() if the stringified class doen not exist', () => {
+    const testValue = new TestSerializeClass1();
+    let serializedValue = serialize(testValue);
+
+    // change the classname
+    serializedValue = serializedValue.split('TestSerializeClass1').join('NonExistingTestSerializeClass');
+
+    // test
+    expect(() => deserialize(serializedValue)).toThrowError('Error in deserialize: \'NonExistingTestSerializeClass\' not decorated as @Serializable()');
+  });
+
+  it('should return an error calling serialize() if the class is not decorated with @Serializable', () => {
+    const testValue = new TestUnserializableClass4();
+
+    expect(() => serialize(testValue)).toThrowError('Error in serialize: class \'TestUnserializableClass4\' not decorated as @Serializable()');
+  });
+});
+
+describe('isSerializable', () => {
+  it('should return true if the object is serializable', () => {
+    // custom class
+    const testValue = new TestSerializeClass1();
+    expect(isSerializable(testValue)).toBe(true);
+
+    // base value
+    const testValue2 = 123;
+    expect(isSerializable(testValue2)).toBe(true);
+  });
+
+  it('should return false if the object is not serializable', () => {
+    const testValue = new TestUnserializableClass4();
+    expect(isSerializable(testValue)).toBe(false);
+  });
+});
---
+++ projects/ngx-simple-serializer/src/lib/serialize.ts
@@ -0,0 +1,131 @@
+import { Type } from '@angular/core';
+
+const serializableClasses = new Map<string, Type<any>>();
+const serializedNameMap = new Map<string, string>();
+
+/**
+ * Class decorator that registers the class as serializable under the specified name
+ */
+export function Serializable(serializedName?: string) {
+  return (serializedClass: Type<any>): void => {
+    serializedName = serializedName ?? serializedClass.name;
+    /* istanbul ignore next */
+    if (serializedNameMap.has(serializedName)) {
+      throw (new Error(`Error in @Serializable() class decorator: '${serializedName}' registered more than once`));
+    }
+    serializedNameMap.set(serializedClass.name, serializedName);
+    serializableClasses.set(serializedName, serializedClass);
+  };
+}
+
+/**
+ * Returns true when the value can be serialized
+ */
+export function isSerializable(value: any) {
+  return serializedNameMap.has(value.constructor.name) ||
+    [Boolean, Number, String, Object, Array, Map, Set, Date, RegExp].indexOf(value.constructor) >= 0;
+}
+
+export const SerializeSymbol = Symbol('serialize');
+export const DeserializeSymbol = Symbol('deserialize');
+
+/**
+ * Interface for a `@Serializable class` to perform custom serialization
+ * Must implement the following methods:
+ * - `[SerializeSymbol](): any`
+ * - `[DeserializeSymbol](serializedValue: any): CustomSerializer` (a class object)
+ */
+export interface CustomSerializer {
+  [SerializeSymbol](): any;
+  [DeserializeSymbol](serializedValue: any): CustomSerializer;
+}
+
+/**
+ * Serialize any value to a JSON string, internally converts Map, Set and Date classes so they can be deserialized correctly.
+ * Also serializes `@Serializable` classes
+ *
+ * @param value value to convert to a JSON string
+ * @returns JSON string
+ */
+export function serialize(value: any): string {
+  // we don't want to use the built in toJSON method of the Date class here, so we remove it temporarily
+  const dateToJSON = Date.prototype.toJSON;
+  // @ts-expect-error deleting non optional operand
+  delete Date.prototype.toJSON;
+  try {
+    return JSON.stringify(value, replacer);
+  } finally {
+    // restore to original toJSON of the Date method
+    Date.prototype.toJSON = dateToJSON;
+  }
+}
+
+/**
+ * Deserialize any value previously serialized with `serialize`.
+ * Deserializes Map, Set and Date classes back to their original state.
+ * Also deserializes `@Persistable` classes
+ *
+ * @param value a JSON string created with `serialize`
+ * @returns the original value
+ */
+export function deserialize(value: string): any {
+  return JSON.parse(value, reviver);
+}
+
+
+// JSON stringify replacer function
+function replacer(key: string, value: any): any {
+  if (key !== '_value') {
+    switch (value?.constructor) {
+      case undefined: return value;
+      case Boolean: return value;
+      case Number: return value;
+      case String: return value;
+      case Object: return value;
+      case Array: return value;
+      case Map: return { _class: 'Map', _value: [...value] };
+      case Set: return { _class: 'Set', _value: [...value] };
+      case Date: return { _class: 'Date', _value: value.getTime() };
+      case RegExp: return { _class: 'RegExp', _value: { source: value.source, flags: value.flags } };
+      default: {
+        const className = serializedNameMap.get(value.constructor.name);
+        if (!className) {
+          throw (new Error(`Error in serialize: class '${value.constructor.name}' not decorated as @Serializable()`));
+        }
+        if (value[SerializeSymbol]) {
+          return { _class: className, _value: value[SerializeSymbol]() };
+        } else {
+          return { _class: className, _value: value };
+        }
+      }
+    }
+  }
+  return value;
+}
+
+// JSON parse reviver function
+function reviver(key: string, value: any): any {
+  if (typeof value === 'object' && value !== null) {
+    switch (value._class) {
+      case 'Map': return new Map(value._value);
+      case 'Set': return new Set(value._value);
+      case 'Date': return new Date(value._value);
+      case 'RegExp': return new RegExp(value._value.source, value._value.flags);
+      default:
+        if (value._class) {
+          const persistedClass = serializableClasses.get(value._class);
+          if (persistedClass) {
+            if (persistedClass.prototype[DeserializeSymbol]) {
+              return persistedClass.prototype[DeserializeSymbol](value._value);
+            } else {
+              const objectProperties = Object.getOwnPropertyDescriptors(value._value);
+              return Object.create(persistedClass.prototype, objectProperties);
+            }
+          } else {
+            throw (new Error(`Error in deserialize: '${value._class}' not decorated as @Serializable()`));
+          }
+        }
+    }
+  }
+  return value;
+}
